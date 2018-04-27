8c86be470c026914606018e4c0baf226f8287196
Alexander Johansson <alexander@n1s.se>
2018-04-27 14:57:00 +0100

    ✨ REST-service setup

--- graphql/index.js
+++ graphql/index.js
@@ -1,5 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const { PORT = 3100, REST_SERVICE_URL = 'http://localhost:3101' } = process.env;
+
 const typeDefs = `
   type Query {
     hello(name: String): String!
@@ -14,4 +16,18 @@ const resolvers = {
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 
-server.start(() => console.log('Server is running on localhost:4000'));
+server
+  .start({
+    port: PORT,
+  })
+  .then(http => {
+    console.log(
+      `> ✅  GraphQL-server is running on http://localhost:${
+        http.address().port
+      }`,
+    );
+  })
+  .catch(() => {
+    console.error('Server start failed', err);
+    process.exit(1);
+  });
